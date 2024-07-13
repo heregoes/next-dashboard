659aa1b74e29f101dc115100c06f8520ebd1aa7f
Itachi <gabnyante@gmail.com>
2024-07-13 18:47:27 -0400

    done with tutorial

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -15,6 +15,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     term ? params.set(' query', term) : params.delete('query');
     replace(`${pathname}?${params.toString()}`);
   }, 300)
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
